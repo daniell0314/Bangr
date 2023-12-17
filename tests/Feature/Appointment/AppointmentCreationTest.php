b384ee9c49fd6597e7a94667c4da33cd5941cdeb
daniell0314 <daniell0314@gmail.com>
2023-12-17 15:42:28 -0500

    update test and add studio model

--- tests/Feature/Appointment/AppointmentCreationTest.php
+++ tests/Feature/Appointment/AppointmentCreationTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\Appointment;
 
 use App\Models\User;
+use App\Models\Appointment;
 use Carbon\Carbon;
 use Faker\Factory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -19,16 +20,24 @@ class AppointmentCreationTest extends TestCase
         $studio = Studio::factory()->create();
         $artist = User::factory()->create();
 
+		$appointment = [
+			'date' => Carbon::now(),
+			'studio_id' => $studio->id,
+			'artist_id' => $artist->id,
+		];
+
         $response = $this
             ->actingAs($user)
-            ->post('/user/' . $user->id . 'appointment', [
-                'date' => Carbon::now(),
-                'studio_id' => $studio->id,
-                'artist_id' => $artist->id,
-            ]);
+            ->post('/user/' . $user->id . 'appointment', $appointment);
 
         $response->assertStatus(201);
-        $response->assertStatus(201);
+		$this->assertDatabaseHas('appointments', $appointment);
 
+
+		$lastAppointment = Appointment::latest()->first();
+
+		$this->assertEquals($appointment['date'], $lastAppointment->date);
+		$this->assertEquals($appointment['studio_id'], $lastAppointment->studio_id);
+		$this->assertEquals($appointment['artist_id'], $lastAppointment->artist_id);
     }
 }
